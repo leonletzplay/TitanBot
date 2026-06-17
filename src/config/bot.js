062c0247be367a9e314667860ffaf2fe63ce0f9b
leonletzplay <contact.leonletzplay@gmail.com>
2026-06-17 10:24:16 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Ich bin ein stinker :)",
         // Activity type number (0 = Playing).
         type: 0, 
       },
